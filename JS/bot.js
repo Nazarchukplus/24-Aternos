ec16e64d43a393afbce60ecb38a94a1c1036feac
Nazarchukplus <109782724+Nazarchukplus@users.noreply.github.com>
2022-07-22 08:43:46 +0300

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -4,13 +4,13 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
+    host: "CK_plus.aternos.me",
+    port: "36426",
     username: "Test",
     version: false
     })
     bot.on('login', function() {
-      bot.chat('/reginster 123123123 123123123')
+      bot.chat('/register 123123123 123123123')
     })
     bot.on('chat', (username, message) => {
       if (username === bot.username) return
